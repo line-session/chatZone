f79a53525a27ebc323ae1a77dcd44a1bd594104b
line-session <linetheboss359@gmail.com>
2025-04-03 16:02:41 +0000

    version finale

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,9 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+// vercel params for deployments
+import { SpeedInsights } from "@vercel/speed-insights/next"
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
